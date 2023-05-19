c505dc928cf4d64492fc5fe8835e6ca0c07cb956
Nilima G <gn@shernet.sheridancollege.ca>
2023-05-19 14:21:58 -0400

    added address variable

--- src/arraysdemo/Student.java
+++ src/arraysdemo/Student.java
@@ -7,6 +7,15 @@ public class Student {
     
   private String sId;
 private String sName;
+private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
  
 public Student(String sId, String sName) {
         this.sId = sId;
