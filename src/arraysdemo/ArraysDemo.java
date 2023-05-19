e269a8ab66c2a6256be2658008052bd8bfd90d28
Nilima G <gn@shernet.sheridancollege.ca>
2023-05-19 13:03:19 -0400

    first commit

---
+++ src/arraysdemo/ArraysDemo.java
@@ -0,0 +1,27 @@
+
+package arraysdemo;
+
+import java.util.Scanner;
+
+
+public class ArraysDemo {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Enter a word");
+        Scanner input = new Scanner(System.in);
+        String myWord = input.nextLine();
+        char[] myLetter = new char[myWord.length()];
+        for(int i =0;i<myLetter.length;i++){
+            myLetter[i]=myWord.charAt(i);
+            System.out.print(myLetter[i]);
+        }//end of for
+        System.out.println("printing in reverse");
+        for(int i= myLetter.length-1;i>=0;i--){
+            System.out.print(myLetter[i]);
+        }
+    }
+    
+}
+
---
+++ src/arraysdemo/StudentList.java
@@ -0,0 +1,17 @@
+
+package arraysdemo;
+
+
+public class StudentList {
+  public static void main(String args[]){
+      Student s1 = new Student("s1","nilima");
+      Student[] myList = new Student[3];
+      myList[0]=s1;
+      myList[1]=new Student("s2","abc");
+      myList[2]=new Student("s3","xyz");
+      
+      for(int i=0;i<myList.length;i++){
+          System.out.println(myList[i].getsName());
+      }
+  }  //end of main
+}//end of class
