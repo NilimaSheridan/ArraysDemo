f74051b8127498210cad275b507066b19e6d5f34
Nilima G <gn@shernet.sheridancollege.ca>
2023-05-26 11:43:25 -0400

    created jworld class

--- src/Practice1/HelloWorld.java
+++ src/Practice1/HelloWorld.java
@@ -4,5 +4,7 @@ package Practice1;
 public class HelloWorld {
  public static void main(String[] args){
    System.out.println("Hello World!!");
+   Jworld j1 = new Jworld("Java World");
+   j1.getWord();
  }   
 }
---
+++ src/Practice1/Jworld.java
@@ -0,0 +1,23 @@
+
+package Practice1;
+
+public class Jworld {
+  private String word;
+
+    public Jworld(String word) {
+        this.word = word;
+    }
+
+    Jworld() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+  
+}
