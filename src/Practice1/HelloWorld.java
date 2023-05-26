a6a1d9ad8c17e663a3d58df0dbd0376817f5cb88
Nilima G <gn@shernet.sheridancollege.ca>
2023-05-26 12:13:45 -0400

    created jworld class

--- src/Practice1/HelloWorld.java
+++ src/Practice1/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
  public static void main(String[] args){
    System.out.println("Hello World!!");
    Jworld j1 = new Jworld("Java World");
-   j1.getWord();
+   j1.setWord("Java World");
+   System.out.println(j1.getWord());
+   
  }   
 }
